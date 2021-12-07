77f536c16fc951fee4c5914cef8353a8b5c85b3a
tsekiguc <tsekiguc@student.42tokyo.jp>
2021-12-07 17:07:16 +0900

    update

--- srcs/command_count_put.c
+++ srcs/command_count_put.c
@@ -6,13 +6,13 @@
 /*   By: tsekiguc <tsekiguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 15:19:14 by tsekiguc          #+#    #+#             */
-/*   Updated: 2021/12/07 16:59:07 by tsekiguc         ###   ########.fr       */
+/*   Updated: 2021/12/07 17:04:55 by tsekiguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	command_count_put(t_game *game)
+void	command_count_put(void)
 {
 	static int	command_count;
 
